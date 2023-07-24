625ce8d851fa580ef70187dc3ad0d2dc12ded1e5
anotherinthefire <rongodfreyultra@gmail.com>
2023-07-24 17:52:03 +0800

    compone

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,159 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+import bgMobile from "./assets/bg-main-mobile.png";
+import bgDesktop from "./assets/bg-main-desktop.png";
+import logo from "./assets/card-logo.svg";
+import tick from "./assets/icon-complete.svg";
+import bgCardFront from './assets/bg-card-front.png';
+import bgCardBack from './assets/bg-card-back.png'
+import format from "date-fns/format";
+
 export default function App() {
+  const [confirmed, setConfirmed] = useState(false);
+  const [name, setName] = useState("");
+  const [cardNumber, setCardNumber] = useState("");
+  const [date, setDate] = useState("01/23");
+  const [cvc, setCvc] = useState("");
+
+  const handleCvcChange = (e) => {
+    const inputValue = e.target.value;
+    const limitedInput = inputValue.slice(0, 3);
+    setCvc(limitedInput);
+  };
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello self!
-    </h1>
-  )
+    <>
+      <section>
+        <div className="absolute -z-10 w-full">
+          <picture>
+            <source media="(min-width: 768px)" srcSet={bgDesktop} />
+            <img src={bgMobile} alt="" className="w-full md:w-1/3" />
+          </picture>
+        </div>
+
+        <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 max-w-7xl mx-auto">
+          <div className="mt-10 mx-5 grid grid-cols-1">
+          <article className="front-card p-5 flex flex-col justify-between absolute top-48 h-60 w-80 bg-center bg-cover bg-no-repeat rounded-xl z-20" 
+          style={{ backgroundImage: `url(${bgCardFront})` }}>
+              <img src={logo} alt="" className="w-20 lg:w-28" />
+
+              <div>
+                <h2 className="text-white text-xl lg:text-3xl mb-6 tracking-widest">
+                  {cardNumber}
+                </h2>
+
+                <ul className="flex items-center justify-between">
+                  <li className="text-white uppercase text-base lg:text-xl tracking-widest">
+                    {name}
+                  </li>
+                  <li className="text-white text-base lg:text-xl tracking-widest">
+                    {format(new Date(date), "MM/yy")}
+                  </li>
+                </ul>
+              </div>
+            </article>
+
+            <article className="back-card absolute right-5 h-60 w-80 rounded-xl lg:ml-20 bg-center bg-cover bg-no-repeat"
+             style={{ backgroundImage: `url(${bgCardBack})` }}>
+              <p className="absolute right-10 text-lg lg:text-xl text-white tracking-widest top-24">
+                {cvc}
+              </p>
+            </article>
+          </div>
+
+          <div className="pt-8 px-5 pb-20">
+            {!confirmed && (
+              <form className="flex flex-col justify-center gap-8 max-w-lg lg:h-screen">
+                <div>
+                  <label htmlFor="cardholder_name">Cardholder Name</label>
+                  <input
+                    type="text"
+                    name="cardholder_name"
+                    id="cardholder_name"
+                    placeholder="e.g. Jane Appleseed"
+                    required
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </div>
+
+                <div>
+                  <label htmlFor="card_number">Card Number</label>
+                  <input
+                    type="text"
+                    name="card_number"
+                    id="card_number"
+                    placeholder="e.g. 1234 5678 9012 3456"
+                    required
+                    maxLength={19}
+                    value={cardNumber
+                      .replace(/\s/g, "")
+                      .replace(/(\d{4})/g, "$1 ")
+                      .trim()}
+                    onChange={(e) => setCardNumber(e.target.value)}
+                  />
+                </div>
+
+                <article className="flex items-center justify-between gap-8">
+                  <div className="flex-1">
+                    <label htmlFor="expiry_date">Exp. Date (MM/YY)</label>
+                    <input
+                      type="month"
+                      name="expiry_date"
+                      id="expiry_date"
+                      placeholder="MM YY"
+                      required
+                      value={date}
+                      onChange={(e) => setDate(e.target.value)}
+                    />
+                  </div>
+
+                  <div className="flex-1">
+                    <label htmlFor="cvc">CVC</label>
+                    <input
+                      type="number"
+                      name="cvc"
+                      id="cvc"
+                      placeholder="e.g. 123"
+                      maxLength={3}
+                      required
+                      value={cvc}
+                      onChange={handleCvcChange}
+                    />
+                  </div>
+                </article>
+
+                <button onClick={() => setConfirmed(true)} className="btn">
+                  Confirm
+                </button>
+              </form>
+            )}
+
+            {confirmed && <ThankYou setConfirmed={setConfirmed} />}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+function ThankYou({ setConfirmed }) {
+  return (
+    <>
+      <div className="thank-you flex flex-col items-center justify-center lg:h-screen max-w-lg mx-auto">
+        <img src={tick} alt="" className="block mx-auto" />
+        <h1 className="text-slate-800 text-3xl my-6 uppercase text-center">
+          Thank you for filling!
+        </h1>
+        <p className="text-slate-400 text-center">
+          This is a frontend project, and I sincerely appreciate your effort in filling out this form. Please refrain from sharing your card information with anyone else.
+        </p>
+        <button
+          onClick={() => setConfirmed(false)}
+          className="btn block mx-auto mt-10 w-full"
+        >
+          Continue
+        </button>
+      </div>
+    </>
+  );
 }
